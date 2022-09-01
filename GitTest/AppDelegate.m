f9912995f204530e4310364a0eeceda3ae5d1d1a
fivewu <602168099@qq.com>
2022-09-01 10:26:26 +0800

    20220901

--- GitTest/AppDelegate.m
+++ GitTest/AppDelegate.m
@@ -34,9 +34,153 @@
    NSNumber *zwsErFen = [self findZWSwithErFen:@[@1,@2] nums2:@[@3,@4]];
     NSLog(@"hhh%f erfen%@",zws,zwsErFen);
     
+    NSLog(@"baoli huiwen----%@",[self getLongestPalindrome:@"babad"]);
+    
+   NSLog(@"MMLLCC----%@",[self MLCLongestPalindrome:@"abcocnpncoccd"]) ;
+    
     return YES;
 }
-
+//马拉车 回文
+- (NSString *)MLCLongestPalindrome:(NSString *)str{
+    //将偶数字符串转换为奇数字符串，通过添加分隔符的方式
+    NSMutableString *sb = [NSMutableString stringWithFormat:@""];
+    
+    for (NSInteger i=0; i<str.length; i++) {
+        [sb appendString:[NSString stringWithFormat:@"#%@",[str substringWithRange:NSMakeRange(i, 1)]]];
+    }
+    NSLog(@"MLC %@",sb);
+    NSInteger n = sb.length;
+    //以每个点为中心点，向外扩展
+    NSMutableArray *rArr = [[NSMutableArray alloc]init];
+    NSInteger axis = -1;//中心点的下标
+    NSInteger  r = -1;//最远位置
+    NSInteger max_len = 0,max_axis = 0;//记录答案
+    
+    for (NSInteger i=0; i<n; i++) {
+        NSInteger i_len = 0;
+        if (r>i) {//当i被r覆盖
+//            NSNumber * = rArr[i_mirror];
+            NSInteger i_mirror = 2*axis - i;
+            NSInteger i_min_len = MIN(r-i, [rArr[i_mirror] integerValue]);
+            i_len = [self expandMLC:sb start:i-i_min_len-1 end:i+i_min_len+1];
+        }else{//否则按照普通的中心开花算法扩散
+            i_len = [self expandMLC:sb start:i-1 end:i+1];
+            
+        }
+        if (i+i_len>r) {//更新 r和 axis
+            r = i+i_len;
+            axis = i;
+        }
+        
+        rArr[i] = @(i_len);
+        
+        if (i_len > max_len) {//更新答案
+            max_len = i_len;
+            max_axis = i;
+        }
+    }
+    //还原  得到结果
+    NSInteger begin = max_axis - max_len, end = max_axis + max_len;
+    NSMutableString *res = [[NSMutableString alloc]init];
+    for (NSInteger i=begin; i<end; i++) {
+        if ([sb characterAtIndex:i]!='#') {
+            [res appendString:[NSString stringWithFormat:@"%@",[sb substringWithRange:NSMakeRange(i, 1)]]];
+        }
+    }
+    return res;
+}
+- (NSInteger)expandMLC:(NSString *)str start:(NSInteger)l end:(NSInteger)r{
+    while (l>=0&&r<str.length&&[str characterAtIndex:l]==[str characterAtIndex:r]) {
+        l--;
+        r++;
+    }
+    
+    return (r-l-2)/2;
+}
+//中心开花 回文
+- (NSString *)XZKHLongestPalindrome:(NSString *)str{
+    NSInteger n = str.length;
+    NSInteger begin=0,end=0;
+    
+    for (NSInteger i=0; i<n; i++) {
+        //需处理回文串为奇数或偶数的情况
+        //偶数
+        NSInteger len = [self expand:str start:i end:i+1];
+        if (2*len>end-begin+1) {
+            begin = i-len+1;
+            end = i+len;
+        }
+        
+        //奇数
+        len = [self expand:str start:i-1 end:i];
+        if (2*len+1>end-begin+1) {
+            begin = i - len;
+            end = i+len;
+        }
+        
+    }
+    return [str substringWithRange:NSMakeRange(begin, end+1)];
+}
+- (NSInteger)expand:(NSString *)str start:(NSInteger)l end:(NSInteger)r{
+    while (l>=0&&r<str.length&&[str characterAtIndex:l]==[str characterAtIndex:r]) {
+        l--;
+        r++;
+    }
+    
+    return (r-l-1);
+}
+//动态规划 回文
+- (void)DTGHLongestPalindrome:(NSString *)str{
+    NSInteger n = str.length;
+    NSMutableArray *f;
+    NSInteger begin=0,end=0;
+    for (NSInteger i=0; i<n; i++) {
+        f[i][i] = [NSNumber numberWithBool:YES];
+        if (i>0&&[str characterAtIndex:i]==[str characterAtIndex:(i-1)]) {
+            f[i-1][i] = [NSNumber numberWithBool:YES];
+            
+            begin = i-1;
+            end = i;
+        }
+    }
+    
+    //从大小为3的开始  不能从2开始枚举，因为求解[i,i+1]时，会使用[i+1,i]这样的无效状态
+    for (NSInteger len = 3; len < n; len++) {
+        for (NSInteger i=0; i+len-1<n; i++) {
+            NSInteger j = i+len-1;
+            NSNumber *fij = f[i+1][j-1];
+            if ([fij boolValue]&&[str characterAtIndex:i]==[str characterAtIndex:j]) {
+                f[i][j] = [NSNumber numberWithBool:YES];
+                begin = i;
+                end = j;
+            }
+        }
+    }
+}
+//枚举 回文
+- (NSString *)getLongestPalindrome:(NSString *)str{
+    
+    NSInteger begin = 0,end = 0;
+    for (NSInteger i=0; i<str.length; i++) {
+        for (NSInteger j=i+1; j<str.length; j++) {
+            //如果当前子串是回文，且长度大于最大长度，则更新起点和终点
+            if ([self isPalindromeStr:str start:i end:j]&&j-i>end-begin) {
+                begin = i;
+                end = j;
+            }
+        }
+    }
+    
+    return [str substringWithRange:NSMakeRange(begin, end+1)];
+}
+- (BOOL)isPalindromeStr:(NSString *)str start:(NSInteger)l end:(NSInteger)r{
+    
+    while (l<r &&[str characterAtIndex:l]==[str characterAtIndex:r]) {
+        l++;
+        r--;
+    }
+    return l>=r;
+}
 - (NSNumber *)findZWSwithErFen:(NSArray *)nums1 nums2:(NSArray *)nums2{
     NSInteger m = nums1.count;
     NSInteger n = nums2.count;
